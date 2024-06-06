25401d881d83bf78a384e8ae4b6ca8185afe08a2
Tyler Clancy <87451522+tylerclancy@users.noreply.github.com>
2024-06-06 11:38:45 -0500

    Add Button component and update Navbar with new navigation links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,49 @@
+import { NAVIGATION } from '@/constants';
+import Image from 'next/image';
 import Link from 'next/link';
+import Button from './Button';
 
 const Navbar = () => {
   return (
     <nav className='border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5'>
-      <Link href='/'>Home</Link>
+      <Link href='/'>
+        <Image
+          src='/stelko-logo.png'
+          alt='Stelko Construction Logo'
+          width={150}
+          height={80}
+        />
+      </Link>
+      <ul className='hidden h-full gap-12 lg:flex'>
+        {NAVIGATION.map((link) => (
+          <li key={link.key}>
+            <Link
+              href={link.href}
+              key={link.key}
+              className='regular-16 text-gray-90 curor-pointer pb-1.5 transition-all hover:font-bold'
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div className='lg:flexCenter hidden '>
+        <Button
+          type='button'
+          title='Contact Us'
+          icon='./mail.svg'
+          variant='btn_yellow'
+        />
+      </div>
+
+      <Image
+        src='/menu.svg'
+        alt='Menu'
+        width={45}
+        height={45}
+        className='lg:hidden inline-block cursor-pointer'
+      />
     </nav>
   );
 };
---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+// Navigation
+export const NAVIGATION = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'gallery', label: 'Gallery' },
+  { href: '/', key: 'careers', label: 'Careers' },
+];
